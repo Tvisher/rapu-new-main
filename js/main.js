2a2673209d997191f254c504b984df32ac7356ea
Ivan Tvisher <Tvisher@mail.ru>
2025-08-14 12:08:18 +0500

    add content

--- js/main.js
+++ js/main.js
@@ -20,3 +20,65 @@ const partnersSlider = new Swiper('.partners-slider', {
     spaceBetween: 0,
     centerInsufficientSlides: 1
 });
+
+
+
+const applicationSlider = new Swiper('.application-slider', {
+    slidesPerView: 1,
+    speed: 1200,
+    spaceBetween: 20,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    navigation: {
+        nextEl: '.application .swiper-button-next',
+        prevEl: '.application .swiper-button-prev',
+    },
+});
+
+
+const mediaSectionSlider = new Swiper('.media-section__slider', {
+    slidesPerView: 'auto',
+    speed: 1200,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: '.media-section .swiper-button-next',
+        prevEl: '.media-section .swiper-button-prev',
+    },
+});
+
+
+
+$(".toggle-item__head").on("click", function () {
+    const parent = $(this).parent('.toggle-item');
+    const toggleBody = parent.find('.toggle-item__body');
+    const innerContent = toggleBody.find('.toggle-item__inner');
+    const itemHeadHeight = $(this).outerHeight();
+
+    if (parent.hasClass('open')) {
+        parent.removeClass('open');
+        parent.removeClass('open-finish');
+        // innerContent.css({
+        //     'margin-top': `0`
+        // });
+        setTimeout(() => {
+            toggleBody.slideUp("slow", function () {
+                console.log('close');
+            });
+        }, 300)
+    } else {
+
+        parent.addClass('open');
+        innerContent.css({
+            'margin-top': `-${itemHeadHeight - 30}px`
+        })
+        toggleBody.slideDown("slow", function () {
+            console.log('close');
+            parent.addClass('open-finish');
+        });
+    }
+
+
+
+});
