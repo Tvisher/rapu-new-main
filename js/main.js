a0c9519f7c38e32d01386b8d9f77b38ed4143ad8
Ivan Tvisher <Tvisher@mail.ru>
2025-10-31 14:14:14 +0500

    add some fix

--- js/main.js
+++ js/main.js
@@ -104,7 +104,135 @@ document.addEventListener('click', (e) => {
     }
 
 
-    if (target.closest('.hot-line__close') || (target.closest('.hot-line-modal') && !target.closest('.hot-line__form'))) {
-        hotLineModal && hotLineModal.classList.remove('show');
+    if (target.closest('.result-message-close') || (target.closest('.result-modal') && !target.closest('.result-message'))) {
+        target.closest('.show').classList.remove('show');
     }
 })
+
+const isEmail = (value) => {
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(String(value).trim());
+};
+
+
+const subscribeForm = document.querySelector('.mailing-section__form');
+const subscribeEmailField = subscribeForm.querySelector('[name="email"]');
+const agree = subscribeForm.querySelector('[name="agree"]');
+const subscribeResModal = document.querySelector('.subscribe-res');
+
+subscribeEmailField.addEventListener('input', (e) => {
+    subscribeEmailField.classList.remove('err')
+})
+
+agree.addEventListener('change', (e) => {
+    agree.classList.remove('err')
+})
+
+subscribeForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (subscribeForm.classList.contains('sending')) return
+    if (!agree.checked) agree.classList.add('err')
+    if (!isEmail(subscribeEmailField.value)) subscribeEmailField.classList.add('err');
+    if (subscribeForm.querySelector('.err')) return;
+
+    subscribeForm.classList.add('sending');
+    $.ajax({
+        url: 'ajax.php',
+        type: 'POST',
+        data: { email: subscribeEmailField.value },
+        success: function (response) {
+            console.log('Ответ сервера:', response);
+            subscribeResModal.classList.add('show');
+            subscribeForm.reset();
+            subscribeForm.classList.remove('sending');
+        },
+        error: function (xhr, status, error) {
+            console.error('Ошибка:', error);
+            console.log('Произошла ошибка при отправке.');
+            subscribeForm.classList.remove('sending');
+        }
+    });
+})
+
+
+
+const hotLineForm = document.querySelector('.hot-line__form');
+const hotLineResult = document.querySelector('.hot-line-result');
+
+const nameField = hotLineForm.querySelector('input[type="text"]');
+const emailField = hotLineForm.querySelector('input[name="email"]');
+const phoneField = hotLineForm.querySelector('input[name="phone"]');
+const messageField = hotLineForm.querySelector('textarea');
+
+[nameField, emailField, phoneField, messageField].forEach(el => {
+    el.addEventListener('input', (e) => {
+        el.classList.remove('error')
+    })
+})
+
+hotLineForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (hotLineForm.classList.contains('sennding')) return;
+
+    const name = nameField.value.trim();
+    const email = emailField.value.trim();
+    const phone = phoneField.value.trim();
+    const message = messageField.value.trim();
+
+
+    let hasError = false;
+
+    // Проверка имени
+    if (name.length < 2 || !/^[a-zA-Zа-яА-ЯёЁ\s-]+$/.test(name)) {
+        nameField.classList.add('error');
+        hasError = true;
+    }
+
+    // Проверка email
+    if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/.test(email)) {
+        emailField.classList.add('error');
+        hasError = true;
+    }
+
+    // Проверка телефона
+    if (!/^[\d\s()+-]{6,20}$/.test(phone)) {
+        phoneField.classList.add('error');
+        hasError = true;
+    }
+
+    // Проверка сообщения
+    if (message.length < 3) {
+        messageField.classList.add('error');
+        hasError = true;
+    }
+
+    if (hasError) return;
+
+
+    hotLineForm.classList.add('sennding');
+    $.ajax({
+        url: 'ajax.php',
+        type: 'POST',
+        data: {
+            name: name,
+            email: email,
+            phone: phone,
+            message: message
+        },
+
+        success: function (response) {
+            console.log('Ответ сервера:', response);
+            hotLineForm.reset();
+            hotLineResult.classList.add('show');
+        },
+        error: function (xhr, status, error) {
+            console.error('Ошибка:', error);
+            console.log('Ошибка при отправке. Попробуйте позже.');
+        },
+        complete: function () {
+            hotLineForm.classList.remove('sennding');
+        }
+    });
+});
+
